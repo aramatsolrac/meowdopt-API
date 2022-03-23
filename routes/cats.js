961c9fb554b1a47c01aa5a486f3825fc295fb788
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-22 18:28:53 -0600

    Create routers:
    - get all cats;
    - get a specific cat;
    - add like;
    - remove like;

--- routes/cats.js
+++ routes/cats.js
@@ -1,32 +1,55 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
-const { v4: uuidv4 } = require("uuid");
 
 //cats routes
+const allCats = JSON.parse(fs.readFileSync("./data/cats.json", "utf-8"));
+const allLikes = JSON.parse(fs.readFileSync("./data/catsLikes.json", "utf-8"));
 
 // get all cats
 router.get("/", (req, res) => {
-  fs.readFile("./data/cats.json", "utf-8", (err, data) => {
-    const allCats = JSON.parse(data);
-    if (err) {
-      res.send("error reading cats data");
-    } else {
-      res.send(allCats);
-    }
-  });
+  try {
+    res.send(allCats);
+  } catch (error) {
+    res.send("Error reading cats data:", error);
+  }
 });
 
 // get a specific cat
 router.get("/:id", (req, res) => {
-  fs.readFile("./data/cats.json", "utf-8", (err, data) => {
-    const allCats = JSON.parse(data);
-    const foundCat = allCats.find((data) => data.id === req.params.id);
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(foundCat);
-    }
+  const foundCat = allCats.find((data) => data.id === req.params.id);
+  try {
+    res.send(foundCat);
+  } catch (error) {
+    res.send("Error finding cat data:", error);
+  }
+});
+
+// add like
+router.post("/:id/like", (req, res) => {
+  const userInput = {
+    isLiked: true,
+    userID: req.body.userID,
+    catID: req.body.catID,
+  };
+  console.log(userInput);
+  allLikes.push(userInput);
+  fs.writeFile("./data/catsLikes.json", JSON.stringify(allLikes), () => {
+    res.json({
+      status: "liked",
+      data: allLikes,
+    });
+  });
+});
+
+// remove like
+router.delete("/:id/remove-like", (req, res) => {
+  const newCatsLikes = allLikes.filter((like) => like.catID !== req.params.id);
+  fs.writeFile("./data/catsLikes.json", JSON.stringify(newCatsLikes), () => {
+    res.json({
+      status: "like removed",
+      data: newCatsLikes,
+    });
   });
 });
 
