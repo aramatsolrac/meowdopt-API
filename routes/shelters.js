c1d30d85e392984993d3172b3f60ee5c106b7b45
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-29 17:01:17 -0600

    Update shelters.js

--- routes/shelters.js
+++ routes/shelters.js
@@ -2,17 +2,12 @@ const express = require("express");
 const fs = require("fs");
 const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
-
-// shelter routes
-const allShelters = JSON.parse(
-  fs.readFileSync("./data/shelters.json", "utf-8")
-);
-
-const allCats = JSON.parse(fs.readFileSync("./data/cats.json", "utf-8"));
+const dataHelper = require("../helpers/dataHelper");
 
 // get all shelters
 router.get("/", (req, res) => {
   try {
+    const allShelters = dataHelper.getAllShelters();
     res.send(allShelters);
   } catch (error) {
     res.send("Error reading shelters data", error);
@@ -21,7 +16,9 @@ router.get("/", (req, res) => {
 
 // get a specific shelters
 router.get("/:id", (req, res) => {
-  const foundShelter = allShelters.find((data) => data.id === req.params.id);
+  const foundShelter = dataHelper
+    .getAllShelters()
+    .find((data) => data.id === req.params.id);
   try {
     res.send(foundShelter);
   } catch (error) {
@@ -31,9 +28,9 @@ router.get("/:id", (req, res) => {
 
 // get all cats of a specific shelter
 router.get("/:id/cats", (req, res) => {
-  const foundShelterCats = allCats.filter(
-    (data) => data.shelterID === req.params.id
-  );
+  const foundShelterCats = dataHelper
+    .getAllCats()
+    .filter((data) => data.shelterID === req.params.id);
   try {
     res.send(foundShelterCats);
   } catch (error) {
@@ -42,13 +39,13 @@ router.get("/:id/cats", (req, res) => {
 });
 
 // get all cats of a specific city
-router.get("/city", (req, res) => {
-  const foundCity = allCats.filter((data) => data.shelterID === req.params.id);
-  try {
-    res.send(foundCity);
-  } catch (error) {
-    res.send("Error finding cats of a city:", error);
-  }
-});
+// router.get("/city", (req, res) => {
+//   const foundCity = allCats.filter((data) => data.shelterID === req.params.id);
+//   try {
+//     res.send(foundCity);
+//   } catch (error) {
+//     res.send("Error finding cats of a city:", error);
+//   }
+// });
 
 module.exports = router;
