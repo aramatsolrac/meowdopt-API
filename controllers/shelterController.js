4188ebaffc21a1e8c3a892e732a5de94c77fd7ae
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 16:40:41 -0600

    Update shelterController.js

--- controllers/shelterController.js
+++ controllers/shelterController.js
@@ -5,7 +5,24 @@ exports.index = (_req, res) => {
     .then((data) => {
       res.status(200).json(data);
     })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Warehouses: ${err}`)
-    );
+    .catch((err) => res.status(400).send(`Error retrieving Shelter: ${err}`));
+};
+
+exports.show = (req, res) => {
+  knex("shelter")
+    .where("id", req.params.id)
+    .first()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Shelter: ${err}`));
+};
+
+exports.cats = (req, res) => {
+  knex("cat")
+    .where("shelter_id", req.params.id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Shelter: ${err}`));
 };
