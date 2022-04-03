43c089bdb11c725bd9446fe7b83f84292bebeea7
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 16:40:35 -0600

    Create catController.js

---
+++ controllers/catController.js
@@ -0,0 +1,45 @@
+const knex = require("knex")(require("../knexfile").development);
+
+exports.index = (_req, res) => {
+  knex("cat")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Cats: ${err}`));
+};
+
+exports.show = (req, res) => {
+  knex("cat")
+    .where("id", req.params.id)
+    .first()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Cat: ${err}`));
+};
+
+exports.like = (req, res) => {
+  knex("catLikes")
+    .insert({
+      isLiked: true,
+      user_id: req.body.user_id,
+      cat_id: req.body.cat_id,
+    })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Cat: ${err}`));
+};
+
+exports.removeLike = (req, res) => {
+  knex("catLikes")
+    .where({
+      user_id: req.body.user_id,
+      cat_id: req.body.cat_id,
+    })
+    .del()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Cat: ${err}`));
+};
