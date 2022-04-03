328c173210d84f70667392caf3ef08df987c2619
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 16:01:47 -0600

    Create catRequests.js

---
+++ seed_data/catRequests.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: "1",
+    cat_id: "22",
+    user_id: "1",
+    name: "Lucas",
+    email: "lucas@gmail.com",
+    status: "Received",
+  },
+  {
+    id: "2",
+    cat_id: "1010",
+    user_id: "1",
+    name: "Lucas",
+    email: "lucas@gmail.com",
+    status: "Received",
+  },
+];
