4a0b70ef49e78214cdb7af9f6b9730af46cf161b
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-04 09:45:11 -0600

    Add bcrypt

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile").development);
+const bcrypt = require("bcrypt");
 
 exports.login = (req, res) => {
   knex("user")
@@ -7,15 +8,21 @@ exports.login = (req, res) => {
       if (!data.length) {
         res.status(404).send("User not found");
         return;
-      }
-
-      if (data[0].password === req.body.password) {
-        res
-          .status(200)
-          .send({ user: JSON.stringify(data[0]), message: "Success" });
       } else {
-        res.status(401).send("Password invalid");
+        return bcrypt
+          .compare(req.body.password, data[0].password)
+          .then((isAuthenticated) => {
+            if (!isAuthenticated) {
+              res.status(401).json({
+                error: "Unauthorized Access!",
+              });
+            } else {
+              res
+                .status(200)
+                .send({ user: JSON.stringify(data[0]), message: "Success" });
+            }
+          })
+          .catch((err) => res.status(400).send(`Error retrieving Cat: ${err}`));
       }
-    })
-    .catch((err) => res.status(400).send(`Error retrieving Cat: ${err}`));
+    });
 };
