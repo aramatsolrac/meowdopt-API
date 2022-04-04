40772d7a005e48ed2a8f9f5d5c1d8a42ab268c3a
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 18:59:10 -0600

    Update shelters.js

--- routes/shelters.js
+++ routes/shelters.js
@@ -1,42 +1,47 @@
-const express = require("express");
-const fs = require("fs");
-const router = express.Router();
-const { v4: uuidv4 } = require("uuid");
-const dataHelper = require("../helpers/dataHelper");
+// const express = require("express");
+// const fs = require("fs");
+// const router = express.Router();
+// const { v4: uuidv4 } = require("uuid");
+// const dataHelper = require("../helpers/dataHelper");
+const router = require("express").Router();
+const shelterController = require("../controllers/shelterController");
 
+router.route("/").get(shelterController.index);
+router.route("/:id").get(shelterController.show);
+router.route("/:id/cats").get(shelterController.cats);
 // get all shelters
-router.get("/", (req, res) => {
-  try {
-    const allShelters = dataHelper.getAllShelters();
-    res.send(allShelters);
-  } catch (error) {
-    res.send("Error reading shelters data", error);
-  }
-});
+// router.get("/", (req, res) => {
+//   try {
+//     const allShelters = dataHelper.getAllShelters();
+//     res.send(allShelters);
+//   } catch (error) {
+//     res.send("Error reading shelters data", error);
+//   }
+// });
 
 // get a specific shelters
-router.get("/:id", (req, res) => {
-  const foundShelter = dataHelper
-    .getAllShelters()
-    .find((data) => data.id === req.params.id);
-  try {
-    res.send(foundShelter);
-  } catch (error) {
-    res.send("Error finding shelter data:", error);
-  }
-});
+// router.get("/:id", (req, res) => {
+//   const foundShelter = dataHelper
+//     .getAllShelters()
+//     .find((data) => data.id === req.params.id);
+//   try {
+//     res.send(foundShelter);
+//   } catch (error) {
+//     res.send("Error finding shelter data:", error);
+//   }
+// });
 
 // get all cats of a specific shelter
-router.get("/:id/cats", (req, res) => {
-  const foundShelterCats = dataHelper
-    .getAllCats()
-    .filter((data) => data.shelterID === req.params.id);
-  try {
-    res.send(foundShelterCats);
-  } catch (error) {
-    res.send("Error finding shelter cats:", error);
-  }
-});
+// router.get("/:id/cats", (req, res) => {
+//   const foundShelterCats = dataHelper
+//     .getAllCats()
+//     .filter((data) => data.shelterID === req.params.id);
+//   try {
+//     res.send(foundShelterCats);
+//   } catch (error) {
+//     res.send("Error finding shelter cats:", error);
+//   }
+// });
 
 // get all cats of a specific city
 // router.get("/city", (req, res) => {
