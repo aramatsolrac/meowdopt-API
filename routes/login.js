2aefa3a2c313810407c2d99f36d54495b2f2a732
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 18:59:02 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,33 +1,37 @@
-const express = require("express");
-const fs = require("fs");
-const router = express.Router();
+// const express = require("express");
+// const fs = require("fs");
+// const router = express.Router();
+const router = require("express").Router();
+const loginController = require("../controllers/loginController");
 
-router.post("/", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+router.route("/").post(loginController.login);
 
-  fs.readFile("./data/users.json", "utf-8", (err, data) => {
-    const allUsers = JSON.parse(data);
+// router.post("/", (req, res) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
 
-    if (err) {
-      res.status(500).send("Error reading users data");
-    } else {
-      const foundUser = allUsers.find((data) => data.username === username);
+//   fs.readFile("./data/users.json", "utf-8", (err, data) => {
+//     const allUsers = JSON.parse(data);
 
-      if (!foundUser) {
-        res.status(404).send("User not found");
-        return;
-      }
+//     if (err) {
+//       res.status(500).send("Error reading users data");
+//     } else {
+//       const foundUser = allUsers.find((data) => data.username === username);
 
-      if (foundUser.password === password) {
-        res
-          .status(200)
-          .send({ user: JSON.stringify(foundUser), message: "Success" });
-      } else {
-        res.status(401).send("Password invalid");
-      }
-    }
-  });
-});
+//       if (!foundUser) {
+//         res.status(404).send("User not found");
+//         return;
+//       }
+
+//       if (foundUser.password === password) {
+//         res
+//           .status(200)
+//           .send({ user: JSON.stringify(foundUser), message: "Success" });
+//       } else {
+//         res.status(401).send("Password invalid");
+//       }
+//     }
+//   });
+// });
 
 module.exports = router;
