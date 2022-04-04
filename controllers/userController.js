ae5866a5cfbb273bf874f0c97285b7c731aa3099
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-03 18:58:46 -0600

    Create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,60 @@
+const knex = require("knex")(require("../knexfile").development);
+
+exports.index = (_req, res) => {
+  knex("user")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving User: ${err}`));
+};
+
+exports.show = (req, res) => {
+  knex("user")
+    .where("id", req.params.id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving User: ${err}`));
+};
+
+exports.favorite = (req, res) => {
+  knex("catLikes")
+    .select(
+      "cat.name as cat_name",
+      "cat.image",
+      "user.name as user_name",
+      "catLikes.*"
+    )
+    .innerJoin("cat", "cat.id", "catLikes.cat_id")
+    .innerJoin("user", "user.id", "catLikes.user_id")
+    .where("user_id", req.params.id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving User: ${err}`));
+};
+
+exports.requests = (req, res) => {
+  knex("catRequests")
+    .select("cat.name as cat_name", "cat.image", "catRequests.*")
+    .innerJoin("cat", "cat.id", "catRequests.cat_id")
+    .where("user_id", req.params.id)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving User: ${err}`));
+};
+
+exports.signup = (req, res) => {
+  knex("user")
+    .insert({
+      name: req.body.name,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error creating User: ${err}`));
+};
